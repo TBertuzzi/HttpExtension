b2ada3a1a3d9a9211266c27ee88282ad8bb780b6
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-02-21 10:53:38 -0300

    :tada: First Version

--- HttpExtension/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HttpExtension
-{
-    public class Class1
-    {
-    }
-}
---
+++ HttpExtension/ServiceResponsecs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace HttpExtension
+{
+    public sealed class ServiceResponse<T>
+    {
+        /// <summary>
+        /// Gets the status code.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; private set; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// Gets the content.
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        public Exception Error { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceResponse{T}"/> class.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <param name="statusCode">
+        /// The status code.
+        /// </param>
+        public ServiceResponse(T value, HttpStatusCode statusCode)
+        {
+            this.Value = value;
+            this.StatusCode = statusCode;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceResponse{T}"/> class.
+        /// </summary>
+        /// <param name="statusCode">
+        /// The status code.
+        /// </param>
+        /// <param name="error">
+        /// The error.
+        /// </param>
+        public ServiceResponse(HttpStatusCode statusCode, Exception error = null)
+        {
+            this.StatusCode = statusCode;
+            this.Error = error;
+        }
+    }
