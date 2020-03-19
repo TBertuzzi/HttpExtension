d544b4970fe7c82fab58cd255d75e678f0c85967
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-03-19 13:46:21 -0300

    :hammer: Refactor Principal Classes. :bug: bug fixes

---
+++ HttpExtension/HttpExtensionHelper.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class HttpExtensionHelper
+{
+    public static async Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string address,
+        object dto, string mediaType = "application/json")
+    {
+        var jsonRequest = JsonConvert.SerializeObject(dto);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
+
+        return await httpClient.PostAsync(address, content);
+    }
+
+
+    public static async Task<HttpExtensionResponse<T>> PostAsync<T>(this HttpClient httpClient, string address,
+        object dto, string mediaType = "application/json")
+    {
+        try
+        {
+            var jsonRequest = JsonConvert.SerializeObject(dto);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
+
+            var response = await httpClient.PostAsync(
+                address,
+                content);
+
+            return await GetResponse<T>(response);
+        }
+        catch (Exception ex)
+        {
+            return new HttpExtensionResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+
+    public static async Task<HttpResponseMessage> PutAsync(this HttpClient httpClient, string address, object dto,
+        string mediaType = "application/json")
+    {
+        var jsonRequest = JsonConvert.SerializeObject(dto);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
+
+        return await httpClient.PutAsync(address, content);
+    }
+
+
+    public static async Task<HttpExtensionResponse<T>> PutAsync<T>(this HttpClient httpClient, string address,
+        object dto, string mediaType = "application/json")
+    {
+        try
+        {
+            var jsonRequest = JsonConvert.SerializeObject(dto);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
+
+            var response = await httpClient.PutAsync(
+                address,
+                content);
+
+            return await GetResponse<T>(response);
+        }
+        catch (Exception ex)
+        {
+            return new HttpExtensionResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+
+    public static async Task<HttpResponseMessage> DeleteAsync(this HttpClient httpClient,
+        string address, object dto, string mediaType = "application/json")
+    {
+        var jsonRequest = JsonConvert.SerializeObject(dto);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
+
+        return await httpClient.DeleteAsync(address, content);
+    }
+
+
+    public static async Task<HttpExtensionResponse<T>> DeleteAsync<T>(this HttpClient httpClient, string address,
+        object dto, string mediaType = "application/json")
+    {
+        try
+        {
+            var jsonRequest = JsonConvert.SerializeObject(dto);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
+
+            var response = await httpClient.DeleteAsync(
+                address,
+                content);
+
+            return await GetResponse<T>(response);
+        }
+        catch (Exception ex)
+        {
+            return new HttpExtensionResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+
+
+    public static async Task<HttpExtensionResponse<T>> GetAsync<T>(this HttpClient httpClient, string address)
+    {
+        try
+        {
+            var response = await httpClient.GetAsync(address).ConfigureAwait(false);
+            return await GetResponse<T>(response);
+        }
+        catch (Exception ex)
+        {
+            return new HttpExtensionResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+
+    public static async Task<HttpExtensionResponse<T>> GetAsync<T>(this HttpClient httpClient, string address,
+        Dictionary<string, string> values)
+    {
+        try
+        {
+            var builder = new StringBuilder();
+
+            foreach (var pair in values)
+            {
+                builder.Append($"&{pair.Key}={pair.Value}");
+            }
+
+            var url = $"{address}?{builder.ToString().Substring(1)}";
+            var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+            return await GetResponse<T>(response);
+        }
+        catch (Exception ex)
+        {
+            return new HttpExtensionResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+
+    private static async Task<HttpExtensionResponse<T>> GetResponse<T>(HttpResponseMessage response)
+    {
+        var returnResponse = new HttpExtensionResponse<T>(response.StatusCode);
+
+        try
+        {
+            returnResponse.Content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (returnResponse.Content != null)
+            {
+                returnResponse.Value = JsonConvert.DeserializeObject<T>(returnResponse.Content);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            returnResponse.Error = ex;
+        }
+
+        return returnResponse;
+    }
+}
---
+++ HttpExtension/HttpExtensionResponse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+
+public sealed class HttpExtensionResponse<T>
+{
+    /// <summary>
+    /// Gets the status code.
+    /// </summary>
+    public HttpStatusCode StatusCode { get; private set; }
+
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    public T Value { get; set; }
+
+    /// <summary>
+    /// Gets the content.
+    /// </summary>
+    public string Content { get; set; }
+
+    /// <summary>
+    /// Gets the error.
+    /// </summary>
+    public Exception Error { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpExtensionResponse{T}"/> class.
+    /// </summary>
+    /// <param name="value">
+    /// The value.
+    /// </param>
+    /// <param name="statusCode">
+    /// The status code.
+    /// </param>
+    public HttpExtensionResponse(T value, HttpStatusCode statusCode)
+    {
+        this.Value = value;
+        this.StatusCode = statusCode;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpExtensionResponse{T}"/> class.
+    /// </summary>
+    /// <param name="statusCode">
+    /// The status code.
+    /// </param>
+    /// <param name="error">
+    /// The error.
+    /// </param>
+    public HttpExtensionResponse(HttpStatusCode statusCode, Exception error = null)
+    {
+        this.StatusCode = statusCode;
+        this.Error = error;
+    }
+}
+
