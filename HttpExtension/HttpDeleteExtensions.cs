c798daab91a0e621b4eb442d4f265aa9b2d43bac
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2021-07-12 10:35:55 -0300

    :sparkles: Adding support for DeleteAsync without body

--- HttpExtension/HttpDeleteExtensions.cs
+++ HttpExtension/HttpDeleteExtensions.cs
@@ -65,12 +65,40 @@ public static partial class HttpExtensions
         });
     }
 
+    public static async Task<HttpExtensionResponse<T>> DeleteAsync<T>(this HttpClient httpClient, string address)
+    {
+        try
+        {
+            return await InternalDeleteAsync<T>(httpClient, address);
+        }
+        catch (Exception ex)
+        {
+            return new HttpExtensionResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+    public static async Task<HttpExtensionResponse<T>> DeleteAsync<T>(this HttpClient httpClient, string address,
+        AsyncRetryPolicy<HttpExtensionResponse<T>> policy)
+    {
+        return await policy.ExecuteAsync(async () =>
+        {
+            return await InternalDeleteAsync<T>(httpClient, address);
+        });
+    }
+
     private static async Task<HttpResponseMessage> InternalDeleteAsync(HttpClient httpClient, string address, object dto, string mediaType)
     {
         var jsonRequest = JsonConvert.SerializeObject(dto);
         var content = new StringContent(jsonRequest, Encoding.UTF8, mediaType);
 
         return await httpClient.DeleteAsync(address, content);
+    }
+
+    private static async Task<HttpExtensionResponse<T>> InternalDeleteAsync<T>(HttpClient httpClient, string address)
+    {
+        var response = await httpClient.DeleteAsync(address, null);
+        return await GetResponse<T>(response);
     }
 
     private static async Task<HttpExtensionResponse<T>> InternalDeleteAsync<T>(HttpClient httpClient, string address, object dto, string mediaType)
